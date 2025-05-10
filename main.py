6ef60468c70e48f14e0d726618b7142ede4d9a3b
ShedrachIkenna <Shedrach686@gmail.com>
2025-05-11 00:42:51 +0100

    Added skills extractor using spacy

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from utils.file_reader import read_resume_text
+from nlp.skill_extractor import extract_skills
 
 def main():
     file_path = 'resume/sample_resume.txt'
     resume_text = read_resume_text(file_path)
     print(f"\n===== Resume Content =====")
     print(resume_text)
+
+    print(f"\n===== Extracted Skills =====")
+    skills = extract_skills(resume_text)
+    print(skills)
 
 
 if __name__ == "__main__":
---
+++ nlp/skill_extractor.py
@@ -0,0 +1,18 @@
+import spacy 
+
+SKILLS_KEYWORD = {
+    "python", "sql", "machine learning", "deep learning", "pandas", "flask",
+    "react", "docker", "aws", "tensorflow", "nlp", "excel", "git", "fastapi"
+}
+
+nlp = spacy.load("en_core_web_sm")
+
+def extract_skills(text: str) -> list:
+    doc = nlp(text.lower())
+    extracted = set()
+
+    for token in doc:
+        if token.text in SKILLS_KEYWORD:
+            extracted.add(token.text)
+
+    return list(extracted)
