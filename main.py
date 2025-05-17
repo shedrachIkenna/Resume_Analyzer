fc9060ea395d7b40c7b0c044b64a17cca8d81893
ShedrachIkenna <Shedrach686@gmail.com>
2025-05-18 00:25:03 +0100

    can read pdf and txt files

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from utils.file_reader import read_resume_text
 from nlp.skill_extractor import extract_skills
 
 def main():
-    file_path = 'resume/sample_resume.txt'
+    file_path = r'C:\Users\DELL\Desktop\smart_resume_analyser\resume\sample_resume.txt'
     resume_text = read_resume_text(file_path)
-    print(f"\n===== Resume Content =====")
+
+    print("\n===== Resume Content =====\n")
     print(resume_text)
 
-    print(f"\n===== Extracted Skills =====")
+    print("\n===== Extracted Skills =====\n")
     skills = extract_skills(resume_text)
     print(skills)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,23 +1,22 @@
-import os 
-import fitz 
+import os
+import fitz  # PyMuPDF
 
 def read_txt(file_path: str) -> str:
     with open(file_path, 'r', encoding='utf-8') as f:
-        f.read()
+        return f.read()
 
 def read_pdf(file_path: str) -> str:
     doc = fitz.open(file_path)
     text = ""
     for page in doc:
         text += page.get_text()
-    return text 
-
+    return text
 
 def read_resume_text(file_path: str) -> str:
     ext = os.path.splitext(file_path)[1].lower()
-    if ext == ".txt":
+    if ext == '.txt':
         return read_txt(file_path)
-    elif ext == ".pdf":
+    elif ext == '.pdf':
         return read_pdf(file_path)
     else:
         raise ValueError(f"Unsupported file type: {ext}")
